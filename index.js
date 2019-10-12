aef7b883c4c55de8a0a49bb8b834756e23e0e077
Jonathan <jkarta26@gmail.com>
2019-10-13 00:03:30 +0300

    update pacman moves

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 
 
-const world= {
-    BRICK: 0,
-    COIN : 1,
-    EMPTY_SQUARE : 2,
+const world = {
+    BRICK:  0,
+    COIN :  1,
+    EMPTY : 2,
     GHOST : 3,
-    PACMAN : 4,
+    PACMAN :4,
 }
-const direction={
-    SPACE:32,
-    LEFT:37,
-    UP: 38,
-    RIGHT:39,
-    DOWN:40,
+const direction = {
+    SPACE:  32,
+    LEFT:   37,
+    UP:     38,
+    RIGHT:  39,
+    DOWN:   40,
 }
-var pacman ={
+
+var pacman = {
     row:15,
     col:12,
 }
+
 var score = 0;
 var keyCode;
+var audio;
 
 var mapWorld = [
-    //0 1 2 3 4 5 6 7 8 9 10 .............................26
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
     [0,1,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0],
@@ -34,7 +36,6 @@ var mapWorld = [
     [2,2,2,0,1,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,2,2,2],
     [2,2,2,0,1,0,1,0,0,2,2,2,2,2,2,2,2,2,0,0,1,0,1,0,2,2,2],
     [0,0,0,0,1,0,1,0,0,2,0,0,0,2,0,0,0,2,0,0,1,0,1,0,0,0,0],
-
     [0,2,2,2,1,1,1,0,0,2,0,0,3,3,3,0,0,2,0,0,1,1,1,2,2,2,0],
     [0,0,0,0,1,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,1,0,1,0,0,0,0],
     [0,1,1,1,1,1,0,0,0,2,2,2,2,2,2,2,2,2,0,0,1,0,1,1,1,1,0],
@@ -48,125 +49,128 @@ var mapWorld = [
 
 
     function createInfoGamePlay(){
-        document.getElementById('pacman-world').innerHTML += "<h1 id='score'> Your Score: " + score + "</h1><br>";
+        document.getElementById('pacman-world').innerHTML += "<h1 id='score'> Your Score: " + score + "</h1><br><br>";
 
-        document.getElementById('pacman-world').innerHTML += "<h1> Your life: </h1>";
+        document.getElementById('pacman-world').innerHTML += "<h1>Your life:</h1>";
         for(var i = 0; i < 3; i = i + 1){
-            document.getElementById('pacman-world').innerHTML += "<div class='pacman'></div>";
+            document.getElementById('pacman-world').innerHTML += "<div class='pacman-life'></div>";
         }
         document.getElementById('pacman-world').innerHTML += "<br>";
     }
 
-function createWorld(mapWorld){
-    document.getElementById('pacman-world').innerHTML = "";
+    function createWorld(mapWorld){
 
-    for (var i = 0; i < mapWorld.length; i = i + 1){
-        for(var j = 0; j < mapWorld[i].length; j = j + 1){
-            switch(mapWorld[i][j]){
-                case world.BRICK:
-                    document.getElementById('pacman-world').innerHTML += "<div class='wall target'></div>";
-                break;
-                case world.COIN:
-                    document.getElementById('pacman-world').innerHTML += "<div class='square target'><div class='coin'</div></div>";
-                break;
-                case world.EMPTY_SQUARE:
-                    document.getElementById('pacman-world').innerHTML += "<div class='square target'></div>";
-                break;
-                case world.GHOST:
-                    document.getElementById('pacman-world').innerHTML +="<div class='ghost target'></div>";
-                break;
-                case world.PACMAN:
-                    document.getElementById('pacman-world').innerHTML += "<div class='pacman target'></div>";
-                break;
-            }
-            if((j+1)%mapWorld[i].length == 0){
+        for (var i = 0; i < mapWorld.length; i = i + 1){
+            for(var j = 0; j < mapWorld[i].length; j = j + 1){
+                switch(mapWorld[i][j]){
+                    case world.BRICK:
+                    document.getElementById('pacman-world').innerHTML += "<div class='wall square'></div>";
+                    break;
+                    case world.COIN:
+                    document.getElementById('pacman-world').innerHTML += "<div class='square'><div class='coin'</div></div>";
+                    break;
+                    case world.EMPTY:
+                    document.getElementById('pacman-world').innerHTML += "<div class='square'></div>";
+                    break;
+                    case world.GHOST:
+                    document.getElementById('pacman-world').innerHTML +="<div class='ghost square'></div>";
+                    break;
+                    case world.PACMAN:
+                    document.getElementById('pacman-world').innerHTML += "<div class='square'><div class='pacman-right'></div></div>";
+                    break;
+                }
+                if((j+1)%mapWorld[i].length == 0){
                     document.getElementById('pacman-world').innerHTML += "<br>";
+                }
             }
         }
+        audio = Audio = new Audio("audio/chomp.wav");
+        createInfoGamePlay();
     }
-    createInfoGamePlay();
-}
 
-function updateScore(){
-    score = score + 10;
-    document.getElementById('score').innerHTML = "Your Score: " + score ;
-}
+    function updateScore(){
+        score = score + 10;
+        document.getElementById('score').innerHTML = "Your Score: " + score ;
+    }
 
-function addClass(className){
-    document.getElementById("pacman-world").getElementsByClassName('target')[pacman.row*27+pacman.col].classList.add(className);
-}
+    function changePacmanDirection(direction){
+        document.getElementById("pacman-world").getElementsByClassName('square')[pacman.row*27+pacman.col].innerHTML = "<div class='pacman-"+ direction +"'></div>";
+    }
 
-function removeClass(className){
-    document.getElementById("pacman-world").getElementsByClassName('target')[pacman.row*27+pacman.col].classList.remove(className);
-}
+    function removePacmanCurrentLoc(){
+        document.getElementById("pacman-world").getElementsByClassName('square')[pacman.row*27+pacman.col].innerHTML = "";
+    }
 
-function changeCoinVisisbility(){
-    document.getElementById("pacman-world").getElementsByClassName('target')[pacman.row*27+pacman.col].getElementsByClassName('coin')[0].style.visibility = "hidden";
-}
+    function changeCoinVisisbility(){
+        document.getElementById("pacman-world").getElementsByClassName('square')[pacman.row*27+pacman.col].getElementsByClassName('coin')[0].style.visibility = "hidden";
+    }
 
 
-function pacmanMove(keyCode){
-    var target_col = 0;
-    var target_row = 0;
-    var validMove = true;
+    function pacmanMove(keyCode){
+        var target_col = 0;
+        var target_row = 0;
+        var validMove = true;
+        var pacmanDirection;
 
-    if(keyCode == direction.RIGHT && mapWorld[pacman.row][pacman.col + 1] != world.BRICK){
-        target_col = pacman.col + 1;
-        target_row = pacman.row;
+        if(keyCode == direction.RIGHT && mapWorld[pacman.row][pacman.col + 1] != world.BRICK){
+            target_col = pacman.col + 1;
+            target_row = pacman.row;
+            pacmanDirection = "right";
 
-    }else if(keyCode == direction.LEFT && mapWorld[pacman.row][pacman.col - 1] != world.BRICK){
-        target_col = pacman.col - 1;
-        target_row = pacman.row;
+        }else if(keyCode == direction.LEFT && mapWorld[pacman.row][pacman.col - 1] != world.BRICK){
+            target_col = pacman.col - 1;
+            target_row = pacman.row;
+            pacmanDirection = "left";
 
-    }else if(keyCode == direction.UP && mapWorld[pacman.row - 1][pacman.col] != world.BRICK){
-        target_row = pacman.row - 1;
-        target_col = pacman.col;
+        }else if(keyCode == direction.UP && mapWorld[pacman.row - 1][pacman.col] != world.BRICK){
+            target_row = pacman.row - 1;
+            target_col = pacman.col;
+            pacmanDirection = "up";
 
-    }else if(keyCode == direction.DOWN && mapWorld[pacman.row + 1][pacman.col] != world.BRICK){
-        target_row = pacman.row + 1;
-        target_col = pacman.col;
+        }else if(keyCode == direction.DOWN && mapWorld[pacman.row + 1][pacman.col] != world.BRICK){
+            target_row = pacman.row + 1;
+            target_col = pacman.col;
+            pacmanDirection = "down";
 
-    }else
+        }else
         validMove = false;
 
-    if(validMove){
-        if(mapWorld[target_row][target_col] == world.EMPTY_SQUARE){
-            mapWorld[pacman.row][pacman.col] = world.EMPTY_SQUARE;
-            removeClass('pacman');
-            addClass('square');
-            pacman.col = target_col;
-            pacman.row = target_row;
-            addClass('pacman');
+        if(validMove){
+            if(mapWorld[target_row][target_col] == world.EMPTY){
+                mapWorld[pacman.row][pacman.col] = world.EMPTY;
+                removePacmanCurrentLoc();
+                pacman.col = target_col;
+                pacman.row = target_row;
+                changePacmanDirection(pacmanDirection);
+            }else{ // it's a coin
+                mapWorld[pacman.row][pacman.col] = world.EMPTY;
+                removePacmanCurrentLoc();
+                pacman.col = target_col;
+                pacman.row = target_row;
+                changeCoinVisisbility();
+                changePacmanDirection(pacmanDirection);
+                updateScore();
+                audio.play();
+            }
 
-        }else{ // it's a coin
-            mapWorld[pacman.row][pacman.col] = world.EMPTY_SQUARE;
-            removeClass('pacman');
-            pacman.col = target_col;
-            pacman.row = target_row;
-            changeCoinVisisbility();
-            addClass('pacman');
-            updateScore();
+            mapWorld[pacman.row][pacman.col] = world.PACMAN;
         }
-        mapWorld[pacman.row][pacman.col] = world.PACMAN;
     }
-}
 
-window.addEventListener("keydown", function(e)  {
-    if([direction.UP, direction.DOWN, direction.LEFT, direction.RIGHT].indexOf(e.keyCode) > -1)
+    window.addEventListener("keydown", function(e)  {
+        if([direction.UP, direction.DOWN, direction.LEFT, direction.RIGHT].indexOf(e.keyCode) > -1)
         keyCode = e.keyCode;
-});
+    });
 
-function gameLoop(){
-    pacmanMove(keyCode);
-    setTimeout(gameLoop,400);
-}
-window.addEventListener("keydown", function(e) {
-    if([direction.SPACE, direction.UP, direction.DOWN, direction.LEFT, direction.RIGHT].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
+    function gameLoop(){
+        pacmanMove(keyCode);
+        setTimeout(gameLoop,300);
     }
-}, false);
+    window.addEventListener("keydown", function(e) {
+        if([direction.SPACE, direction.UP, direction.DOWN, direction.LEFT, direction.RIGHT].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
 
-
-createWorld(mapWorld);
-
-gameLoop();
+    createWorld(mapWorld);
+    gameLoop();
